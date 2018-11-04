9659fa482c047f6d0741715062cbb1e12d74d756
Partha <partha187@gmail.com>
2018-11-04 17:19:52 +0530

    updated beans

---
+++ src/main/java/com/support/diksha/domain/jira/Progress.java
@@ -0,0 +1,18 @@
+package com.support.diksha.domain.jira;
+
+public class Progress {
+	private Float progress;
+	private Float total;
+	public Float getProgress() {
+		return progress;
+	}
+	public void setProgress(Float progress) {
+		this.progress = progress;
+	}
+	public Float getTotal() {
+		return total;
+	}
+	public void setTotal(Float total) {
+		this.total = total;
+	}
+}
